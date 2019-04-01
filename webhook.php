6cfdeaac809ea66e6d9d723d548393e973186076
Xiaolei Yang <xuanyuan95@126.com>
2019-04-02 00:43:44 +0800

    第一次提

---
+++ webhook.php
@@ -0,0 +1,18 @@
+<?php
+// GitHub Webhook Secret.
+// Keep it the same with the 'Secret' field on your Webhooks / Manage webhook page of your respostory.
+$secret = "gittest";
+// Path to your respostory on your server.
+// e.g. "/var/www/respostory"
+$path = "/www/wwwroot/git.5cflame.com";
+// Headers deliveried from GitHub
+$signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
+if ($signature) {
+  $hash = "sha1=".hash_hmac('sha1', file_get_contents("php://input"), $secret);
+  if (strcmp($signature, $hash) == 0) {
+    echo shell_exec("cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1");
+    exit();
+  }
+}
+http_response_code(404);
+?>
